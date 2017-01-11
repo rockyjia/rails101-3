17c9a00d7d7a03822e05fd46e42eb5475f9bd263
rockyjia <jrk1987@gmail.com>
2017-01-11 22:52:46 +0800

    implenent groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,27 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    @group =Group.find(params[:id])
 
   end
+
 end
