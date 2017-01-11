b01d5ada9da2105cc8130fe62223aee62394d688
rockyjia <jrk1987@gmail.com>
2017-01-11 23:42:53 +0800

    implement groups#edit and destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,16 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "update success"
+    else
+      render :edit
+    end
+
+  end
+
   def destroy
     @group =Group.find(params[:id])
 
